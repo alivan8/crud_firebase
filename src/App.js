3a17de1a240bb79dcf287d2bd813797192634b26
alivan <calle.coymbra@gmail.com>
2019-06-22 18:50:19 -0400

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./App.css";
+import firebase from "./Firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.ref = firebase.firestore().collection("coments");
+    this.unsubscribe = null;
+    this.state = {
+      coments: []
+    };
+  }
+
+  onCollectionUpdate = querySnapshot => {
+    const coments = [];
+    querySnapshot.forEach(doc => {
+      const { title, description, author } = doc.data();
+      coments.push({
+        key: doc.id,
+        doc, // DocumentSnapshot
+        title,
+        description,
+        author
+      });
+    });
+    this.setState({
+      coments
+    });
+  };
+
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+  }
+
+  render() {
+    return (
+    <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                  <h3 class="panel-title">
+                         COMENT LIST
+                   </h3>
+            </div>
+        
+        <div class="panel-body">
+            <h4><Link to="/create">Add Coment</Link></h4>
+                <table class="table table-stripe">
+                  <thead>
+                      <tr>
+                      <th>Title</th>
+                      <th>Description</th>
+                      <th>Author</th>
+                      </tr>
+                  </thead>
+                    <tbody>
+                        {this.state.coments.map(Coment =>
+                            <tr>
+
+                              <td><Link to={`/show/${Coment.key}`}>{Coment.title}</Link></td>
+                              <td>{Coment.description}</td>
+                              <td>{Coment.author}</td>
+                            </tr>
+                        )}
+                  </tbody>
+                </table>
+        </div>
     </div>
-  );
+  </div>
+);
 }
-
+}
 export default App;
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import App from './App'
+import Edit from './componentes/edit'
+import Create from './componentes/create'
+import Show from './componentes/show'
+import Appcss from './App.css'
+import {BrowserRouter,Switch} from 'react-router-dom'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import RegisterServiceWorker from './registerServiceWorker'
+
+
+
+
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path='/' component={App}    />
+            <Route path='/edit/:id' component={Edit} />
+            <Route path='/create' component={Create} />
+            <Route path='/show/:id' component={Show} />
+        </div>
+    </Router>,
+    document.getElementById('root')
+
+)
+
+//RegisterServiceWorker();
